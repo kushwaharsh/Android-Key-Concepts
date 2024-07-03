60294960ab2d14d759d12dbb499518f0cb24e207
Harsh Raj Kushwaha <120735074+kushwaharsh@users.noreply.github.com>
2024-07-03 14:22:25 +0530

    Delete CoonectingAPIwithSQLite directory

--- CoonectingAPIwithSQLite/app/src/main/java/com/example/coonectingapiwithsqlite/Dimensions.kt
+++
@@ -1,7 +0,0 @@
-package com.example.coonectingapiwithsqlite
-
-data class Dimensions(
-    val depth: Double,
-    val height: Double,
-    val width: Double
-)
--- CoonectingAPIwithSQLite/app/src/main/java/com/example/coonectingapiwithsqlite/MyData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.coonectingapiwithsqlite
-
-data class MyData(
-    val products: List<Product>,
-)
